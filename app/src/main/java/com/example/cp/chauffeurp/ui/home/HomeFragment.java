06b2f0db3515076e949d26bae96f2943c3c3c9a6
Fanilo GABAUD <fanilogabaud@yahoo.com>
2018-01-09 11:15:03 +0100

    add null checker

--- app/src/main/java/com/example/cp/chauffeurp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cp/chauffeurp/ui/home/HomeFragment.java
@@ -4,10 +4,8 @@ import android.animation.ObjectAnimator;
 import android.animation.TypeEvaluator;
 import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -135,7 +133,9 @@ public class HomeFragment extends BaseFragment<HomePresenter> implements HomeVie
             moveToPosition(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()));
         }
 
-        mapboxMap.setOnCameraIdleListener(this);
+        if (mapboxMap != null) {
+            mapboxMap.setOnCameraIdleListener(this);
+        }
     }
 
     private void updateDestinationMakerPosition() {
